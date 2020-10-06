17f273c6418f5d493325dc993e8d38cb9838a4a2
HW0001 <1600860624@qq.com>
2020-10-06 20:08:37 +0800

    threeupdate

--- src/index.js
+++ src/index.js
@@ -1,2 +1,6 @@
 import "./css/index.css"
+import "./css/index2.scss"
+import './css/index3.less'
+import "./css/index4.styl"
+
 console.log("hhh");
--- webpack.config.base.js
+++ webpack.config.base.js
@@ -11,5 +11,44 @@ module.exports = {
     title: '今天天气不错',
     filename: 'index.html',
     template:"src/assets/index.html"
-  })]
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.less$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "less-loader" // compiles Less to CSS
+        }]
+    },
+    {
+      test: /\.styl$/,
+      use: [
+        {
+          loader: "style-loader"
+        },
+        {
+          loader: "css-loader"
+        },
+        {
+          loader: "stylus-loader"
+        }
+      ]
+    }
+    ]
+  }
 };
