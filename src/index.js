a7869ef32c457da921652385ac2f2c4e66ad28f7
HW0001 <1600860624@qq.com>
2020-10-06 21:51:33 +0800

    fourupdate

--- src/index.js
+++ src/index.js
@@ -2,5 +2,21 @@ import "./css/index.css"
 import "./css/index2.scss"
 import './css/index3.less'
 import "./css/index4.styl"
+import png from "./assets/1.png" 
 
-console.log("hhh");
+
+console.log(png);
+
+let div = document.getElementById("app")
+div.innerHTML=`
+<img src='${png}'>
+`
+let btn = document.createElement("button")
+btn.innerText="懒加载"
+btn.onclick=function(){
+    const lazy = import("./lazy")
+    lazy.then((module)=>{
+        module.default()
+    })
+}
+div.appendChild(btn)
