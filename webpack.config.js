1722d4158641e731263240c273890b623d3b484a
HW0001 <1600860624@qq.com>
2020-10-06 17:36:02 +0800

    init

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require('path');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.js',
+  mode:"development",//"production",
+  output: {
+    filename: '[name].[contenthash].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+  plugins: [new HtmlWebpackPlugin({
+    title: '今天天气不错',
+    filename: 'index.html',
+    template:"src/assets/index.html"
+  })],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
